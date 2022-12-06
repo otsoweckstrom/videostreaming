cc91d0d75bd18ec301a70fd698096227a82f963d
Otso Weckström <otso.weckstrom@visma.com>
2022-12-06 16:21:29 +0200

    add buttons and forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Header from './components/Header';
-import Video from './components/Video';
 
 function App() {
+  const [movieName, setMovieName] = useState('');
+
+  const [review, setReview] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <Header></Header>
-        <Video></Video>
+        <label>Movie name:</label>
+        <input type="text" name="movieName" />
+        <label>review:</label>
+        <input type="text" name="review" />
+        <button>Submit</button>
       </header>
     </div>
   );
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,17 +1,10 @@
 import ReactPlayer from 'react-player';
-import video1 from './Dancin.mp4';
 
 const Video = () => {
   return (
     <div className="player-wrapper">
       {' '}
-      <ReactPlayer
-        url={video1}
-        width="50%"
-        height="50%"
-        controls={true}
-      ></ReactPlayer>
-      ;
+      <ReactPlayer width="50%" height="50%" controls={true}></ReactPlayer>;
     </div>
   );
 };
