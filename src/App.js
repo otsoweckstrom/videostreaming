966c276aa0597fb3b376f24a44b1bab7d7bd5d47
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-12-12 03:36:22 +0200

    add movie card

--- src/App.js
+++ src/App.js
@@ -2,37 +2,28 @@ import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import './App.css'
 import Header from './components/Header'
-import Country1 from './pages/Country1'
-import Country2 from './pages/Country2'
-import Country3 from './pages/Country3'
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
+    Button,
+} from '@mui/material'
 
 function App() {
     const [country, setCountry] = useState('')
     const [MovieList, setMovieList] = useState([])
 
-    const handleFinlandClick = () => {
-        setCountry('finland')
+    const handleCountryClick = (country) => {
+        setCountry(country)
+
         setMovieList([])
-        Axios.get('http://localhost:3001/api/get/finland').then((response) => {
-            setMovieList(response)
-        })
-        console.log(MovieList)
-    }
-    const handleSpainClick = () => {
-        setCountry('spain')
-        setMovieList([])
-        Axios.get('http://localhost:3001/api/get/spain').then((response) => {
-            setMovieList(response)
-        })
-        console.log(MovieList)
-    }
-    const handleUSAClick = () => {
-        setCountry('usa')
-        setMovieList([])
-        Axios.get('http://localhost:3001/api/get/usa').then((response) => {
-            setMovieList(response)
-        })
-        console.log(MovieList)
+        Axios.get('http://localhost:3001/api/get/' + country).then(
+            (response) => {
+                setMovieList(response)
+            }
+        )
     }
 
     return (
@@ -41,51 +32,53 @@ function App() {
                 <Header></Header>
             </div>
             <div className="Buttons">
-                <button onClick={() => handleFinlandClick()}>Finland</button>
-                <button onClick={() => handleSpainClick()}>Spain</button>
-                <button onClick={() => handleUSAClick()}>USA</button>
+                <button onClick={() => handleCountryClick('finland')}>
+                    Finland
+                </button>
+                <button onClick={() => handleCountryClick('spain')}>
+                    Spain
+                </button>
+                <button onClick={() => handleCountryClick('usa')}>USA</button>
             </div>
-            <div className="movielist">
-                {country === 'finland' && (
-                    <div className="movielist">
-                        {MovieList.data?.length > 0 &&
-                            MovieList.data.map((val) => {
-                                return (
-                                    <div>
-                                        <h1>Title: {val.title}</h1>
-                                    </div>
-                                )
-                            })}
-                        <h1 className="movietitle">Title</h1>
-                        <h1 className="movietitle">Title</h1>
-                    </div>
-                )}
-                {country === 'spain' && (
-                    <div>
-                        {MovieList.data?.length > 0 &&
-                            MovieList.data.map((val) => {
-                                return (
-                                    <div>
-                                        <h1>Title: {val.title}</h1>
-                                    </div>
-                                )
-                            })}
-                        <h1>Title</h1>
-                    </div>
-                )}
-                {country === 'usa' && (
-                    <div>
-                        {MovieList.data?.length > 0 &&
-                            MovieList.data.map((val) => {
-                                return (
-                                    <div>
-                                        <h1>Title: {val.title}</h1>
-                                    </div>
-                                )
-                            })}
-                    </div>
-                )}
-            </div>
+
+            {country !== '' && (
+                <div className="movielist">
+                    {MovieList.data?.length > 0 &&
+                        MovieList.data.map((val) => {
+                            return (
+                                <div className="movie">
+                                    <Card sx={{ width: 220 }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="140"
+                                            image="/images/placeholder.jpg"
+                                            alt="movieposter"
+                                        />
+                                        <CardContent>
+                                            <Typography
+                                                gutterBottom
+                                                variant="h5"
+                                                component="div"
+                                            >
+                                                {val.title}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="text.secondary"
+                                            >
+                                                {console.log(val)}
+                                                {val.summary}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small">Share</Button>
+                                        </CardActions>
+                                    </Card>
+                                </div>
+                            )
+                        })}
+                </div>
+            )}
         </div>
     )
 }
